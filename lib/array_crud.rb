0fe0a9ba1194e4bcb889ffd35ca6c689d06f0dd1
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-08 00:57:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,51 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+letter_array=["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ letter_array=["a", "b", "c"]
+ letter_array.push("arrays!")
+ p letter_array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
+  p array
 end
 
 def remove_element_from_end_of_array(array)
-  
+array = ["I", "am", "really", "learning", "arrays!"]
+array.pop("arrays!")
+p array
 end
 
 def remove_element_from_start_of_array(array)
-  
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.shift("wow")
+p arrray
 end
+
 
 def retrieve_element_from_index(array, index_number)
-  
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[2]
+p array
 end
 
+
 def retrieve_first_element_from_array(array)
-  
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array[0]
+p array
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
 
 def update_element_from_index(array, index_number, element)
